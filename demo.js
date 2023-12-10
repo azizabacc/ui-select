2f1cf4bf9f38b36082d20c09358495d2bf1804e1
azizabacc <azizabaccouri@gmail.com>
2023-12-10 18:04:13 +0100

    ui-select

---
+++ demo.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var app = angular.module('demo', ['ngSanitize', 'ui.select']);
+
+/**
+ * AngularJS default filter with the following expression:
+ * "person in people | filter: {name: $select.search, age: $select.search}"
+ * performs a AND between 'name: $select.search' and 'age: $select.search'.
+ * We want to perform a OR.
+ */
+app.filter('propsFilter', function() {
+  return function(items, props) {
+    var out = [];
+
+    if (angular.isArray(items)) {
+      items.forEach(function(item) {
+        var itemMatches = false;
+
+        var keys = Object.keys(props);
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop].toLowerCase();
+          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+            itemMatches = true;
+            break;
+          }
+        }
+
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    } else {
+      // Let the output be the input untouched
+      out = items;
+    }
+
+    return out;
+  };
+});
+
+app.controller('DemoCtrl', function($scope, $http, $timeout) {
+  $scope.disabled = undefined;
+  $scope.searchEnabled = undefined;
+
+  $scope.enable = function() {
+    $scope.disabled = false;
+  };
+
+  $scope.disable = function() {
+    $scope.disabled = true;
+  };
+  $scope.clearInput = function ($select) {
+    $select.search = ''; // Efface l'entrée dans l'input
+  };
+  $scope.clear = function() {
+    $scope.multipleDemo.colors = [];
+};
+
+$scope.reset = function() {
+    $scope.multipleDemo.colors = [];
+    $scope.placeholderText = "";
+};
+
+
+
+
+
+
+
+  $scope.counter = 0;
+  $scope.someFunction = function (item, model){
+    $scope.counter++;
+    $scope.eventResult = {item: item, model: model};
+  };
+
+
+
+  $scope.availableColors = ['Red','Green','Blue','Yellow','Magenta','Maroon','Umbra','Turquoise'];
+
+  $scope.multipleDemo = {};
+  $scope.multipleDemo.colors = ['Blue','Red'];
+
+
+
+  
+
+});
